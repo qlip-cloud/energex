5f59ba648e4a7b4008d0b04dcb406ba4f19b8625
HendersonMentum <henderson.villegas@mentum.group>
2025-05-02 13:22:52 +0000

    add changes to Quotations Opportunities

---
+++ energex/overrides/quotationEnergex.py
@@ -0,0 +1,18 @@
+import frappe
+from erpnext.erpnext.selling.doctype.quotation.quotation import Quotation
+
+class QuotationEnergex(Quotation):
+
+    def update_opportunity_status(self, status, opportunity=None):
+        if not opportunity:
+            opportunity = self.opportunity
+
+        opp = frappe.get_doc("Opportunity", opportunity)
+
+        if self.currency == 'USD':
+            opp.opportunity_amount = self.base_total
+        else:
+            opp.opportunity_amount = self.total
+            
+        opp.set_status(status=status, update=True)
+    
