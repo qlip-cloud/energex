f9ac0b0d2825464fc05757bb1cf979d2c655d5a1
HendersonMentum <henderson.villegas@mentum.group>
2025-05-02 14:30:54 +0000

    add changes to Quotations Opportunities

--- energex/overrides/quotationEnergex.py
+++ energex/overrides/quotationEnergex.py
@@ -4,6 +4,8 @@ from erpnext.selling.doctype.quotation.quotation import Quotation
 class QuotationEnergex(Quotation):
 
     def update_opportunity_status(self, status, opportunity=None):
+        
+        super(QuotationEnergex, self).update_opportunity_status()
         if not opportunity:
             opportunity = self.opportunity
 
@@ -15,4 +17,5 @@ class QuotationEnergex(Quotation):
             opp.opportunity_amount = self.total
             
         opp.set_status(status=status, update=True)
+        opp.save()
     
